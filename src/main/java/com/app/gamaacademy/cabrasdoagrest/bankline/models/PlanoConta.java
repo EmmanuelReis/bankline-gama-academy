be6b9e5db3ec3adcdd908404bf0f54de73924187
Emmanuel Reis <emmanu_reis@hotmail.com>
2021-03-10 22:42:32 -0300

    new: Implementando model transação, update: Plano de conta para Enum

--- src/main/java/com/app/gamaacademy/cabrasdoagrest/bankline/models/PlanoConta.java
+++ src/main/java/com/app/gamaacademy/cabrasdoagrest/bankline/models/PlanoConta.java
@@ -1,14 +1,15 @@
 package com.app.gamaacademy.cabrasdoagrest.bankline.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+public enum PlanoConta {
+	RECEITA("R"), DESPESA("D"), TRANSFERENCIA("T");
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class PlanoConta {	
-	private int id;
-	private String sigla;
-	private String nome;
+	private String code;
+
+    private PlanoConta(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
