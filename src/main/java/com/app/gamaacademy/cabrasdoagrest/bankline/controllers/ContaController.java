c045f767566398578e063b8915a8c74905368ebc
gloureiro100 <gloureiro100@gmail.com>
2021-03-20 17:42:39 -0300

    extrato por data

--- src/main/java/com/app/gamaacademy/cabrasdoagrest/bankline/controllers/ContaController.java
+++ src/main/java/com/app/gamaacademy/cabrasdoagrest/bankline/controllers/ContaController.java
@@ -1,10 +1,14 @@
 package com.app.gamaacademy.cabrasdoagrest.bankline.controllers;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.gamaacademy.cabrasdoagrest.bankline.dtos.ExtratoDTO;
@@ -20,6 +24,16 @@ public class ContaController {
 	@GetMapping("/{numero}")
 	public ResponseEntity<ExtratoDTO> obter(@PathVariable(value = "numero") Long numero) throws Exception {
 		ExtratoDTO ret = service.extrato(numero);
+
+		return ResponseEntity.ok().body(ret);
+	}
+
+	@GetMapping("/{numero}/detalhado")
+	public ResponseEntity<ExtratoDTO> obterPorData(@PathVariable(value = "numero") Long numero,
+			@RequestParam("dtInicio") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dtInicio,
+			@RequestParam(value = "dtFim", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate dtFim)
+			throws Exception {
+		ExtratoDTO ret = service.extrato(numero, dtInicio, dtFim);
 
 		return ResponseEntity.ok().body(ret);
 	}
--- src/main/java/com/app/gamaacademy/cabrasdoagrest/bankline/repository/TransacaoRepository.java
+++ src/main/java/com/app/gamaacademy/cabrasdoagrest/bankline/repository/TransacaoRepository.java
@@ -3,11 +3,15 @@ package com.app.gamaacademy.cabrasdoagrest.bankline.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.app.gamaacademy.cabrasdoagrest.bankline.models.Transacao;
 
 public interface TransacaoRepository extends JpaRepository<Transacao, Integer> {
 
 	public List<Transacao> findByContaOrigemNumeroEquals(Long numero);
+
+	@Query(value = "select * from bankline.transacao where numero_conta_origem = ?1 and data between ?2 and ?3", nativeQuery = true)
+	public List<Transacao> obterExtrato(Long numero, String dtInicio, String dtFim);
 
 }
